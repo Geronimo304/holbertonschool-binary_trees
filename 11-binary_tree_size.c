3cda8e45192d2fce85a672c863ff067c94e12eda
Agustin800 <aguslahalo@gmail.com>
2025-04-09 12:22:16 +0000

    ejercicio 11,  tasize de un arbol binario

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - Measures the size of a binary tree.
+ * @tree: Pointer to the root node of the tree.
+ * Return: Size (number of nodes), or 0 if tree is NULL.
+ */
+
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	/*Suma 1 por la raiz del arbol y luego suma el sub-arbol izquierdo
+	 * y luego el sub-arbol derecho, eso de el tamañ */
+	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
+}
