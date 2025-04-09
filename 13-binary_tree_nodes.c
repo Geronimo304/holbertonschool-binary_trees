6d17a9b6e25c5efe543bb4df65c98462b090498c
geronimo304 <geronimonegreira06@gmail.com>
2025-04-09 17:22:21 +0000

    trabajo finalizado

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,19 +1,17 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_nodes - cuenta los nodos
+ * binary_tree_nodes - cuenta el numero de nodos
  * @tree: Puntero
- *
- * Return: retorna la cantidad de hijos
+ * Return: numero de nodos
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left == NULL || tree->right == NULL)
-	return (1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
+	if (tree->left != NULL || tree->right != NULL)
+		return (1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
 
-
-	return (0);
+	return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
 }
